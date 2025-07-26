492cea5eb2d2f47cb69fbef424d691c1a29af4aa
Sandro Hatzfeld <69820567+SandroHatzfeld@users.noreply.github.com>
2025-07-26 18:21:35 +0200

    cleanup

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
-// Import env file, install dotenv npm package first
 require("dotenv").config()
 
-const { randomUUID } = require('crypto')
 // Import express Server
 const express = require('express')
 const app = express()
 const path = require('path')
+
 const messagesDetailsRouter = require('./routes/messageDetailsRouter.js')
 const newMessageRouter = require('./routes/newMessageRouter.js')
 const indexRouter = require('./routes/indexRouter.js')
@@ -14,17 +13,12 @@ app.use(express.urlencoded({ extended: true }))
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
-// const indexRouter = require('./routes/indexRouter.js')
-// const newMessageRouter = require('./routes/newMessageRouter.js')
 
 
 app.use("/", indexRouter)
 app.use("/messages", messagesDetailsRouter)
 app.use("/new", newMessageRouter)
 
-
-
-// app.use("/new", newMessageRouter)
 
 // load PORT from env and set fallback port
 const PORT = process.env.PORT || 3000
