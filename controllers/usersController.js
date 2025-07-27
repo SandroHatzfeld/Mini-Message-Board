29c7e101f2d003bbccb21c2880d5b14d2c2bc5c1
Sandro Hatzfeld <69820567+SandroHatzfeld@users.noreply.github.com>
2025-07-27 14:26:21 +0200

    validation using express-validate

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,18 @@
 const userStorage = require("../storages/userStorage.js")
+const { body, validationResult } = require("express-validator")
+
+const alphaErr = "must only contain letters."
+const lengthErr = "must be between 1 and 10 charakters."
+
+const validateUser = [
+	body("firstName").trim()
+		.isAlpha().withMessage(`First name ${alphaErr}`)
+		.isLength({ min: 1, max: 10 }).withMessage(`First name ${lengthErr}`),
+	body("lastName").trim()
+		.isAlpha().withMessage(`Last name ${alphaErr}`)
+		.isLength({ min: 1, max: 10 }).withMessage(`Last name ${lengthErr}`),
+]
+
 
 exports.usersListGet = (req, res) => {
 	res.render("listUsers", {
@@ -11,12 +25,24 @@ exports.usersCreateGet = (req, res) => {
 		title: "Create a new user"
 	})
 }
-exports.usersCreatePost = (req, res) => {
-	const { firstName, lastName } = req.body
-	userStorage.addUser({firstName, lastName})
-	res.redirect("/users")
-}
-exports.usersUpdateGet = (req, res) => {}
-exports.usersUpdatePost = (req, res) => {}
+exports.usersCreatePost = [
+	validateUser,
+	(req, res) => {
+		const errors = validationResult(req)
+		if (!errors.isEmpty()) {
+			return res.status(400).render("createUser", {
+				title: "Create a new user",
+				errors: errors.array()
+			})
+		}
+
+		const { firstName, lastName } = req.body
+		userStorage.addUser({ firstName, lastName })
+		res.redirect("/users")
+	}
+]
+
+exports.usersUpdateGet = (req, res) => { }
+exports.usersUpdatePost = (req, res) => { }
 
 // module.exports = userController
